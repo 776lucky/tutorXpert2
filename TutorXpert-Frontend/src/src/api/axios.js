4f6a3ac6fe9052ce1148cfe02b805ebd067e443a
776lucky <zhuangwenyuan7@gmail.com>
2025-05-29 22:58:53 +1000

    add _redirects

--- TutorXpert-Frontend/src/src/api/axios.js
+++ TutorXpert-Frontend/src/src/api/axios.js
@@ -2,15 +2,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:10000'
-      : 'https://tutorxpert-backend-9qxd.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
   }
 })
-
-
-export default api
